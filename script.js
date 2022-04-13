34f1f84d359ec1da20e87467acb8aad13813cc5c
Aechelon-Online <info@aechelon.online>
2022-04-12 20:15:52 -0500

    Main files for Brewery API

---
+++ script.js
@@ -0,0 +1,44 @@
+
+const selection = document.querySelector(".choose-city")
+
+async function listings() {
+  const cityRes = await fetch('https://api.openbrewerydb.org/breweries?by_state=texas&per_page=100')
+  const cityData = await cityRes.json()
+  let newCity = cityData.map(item => item.city).sort()
+  let newCityArr = [...new Set(newCity)]
+
+  for (i = 0; i < newCityArr.length; i++) {
+  selection.innerHTML +=  
+  `<option class="pick" value="${newCityArr[i]}">${newCityArr[i]}</option>`
+  }
+
+}
+listings()
+
+
+selection.addEventListener("change", () => {
+
+async function main() {
+  const res = await fetch('https://api.openbrewerydb.org/breweries?by_city=' + selection.value + '&per_page=50')
+  const data = await res.json()
+  
+  
+    for (i = 0; i < data.length; i++) {
+      console.log(data[i]['city'])
+    }
+    console.log(selection.value)
+
+  const cl = s => s?s:""
+  document.querySelector(".main").innerHTML = data.map(values => 
+    `<div class="listing">
+        <h1 class="name">${values.name}</h1>
+        <h2 class="street"><a class="google-map" target="_blank" href="https://www.google.com/maps/search/?api=1&query=${cl(values.street)},${cl(values.city)},${values.state} ${values.postal_code}">${cl(values.street)}<br>${cl(values.city)}, ${values.state} ${values.postal_code}</a></h2>
+        <h2><a class="phone-tag" href="tel:${values.phone}">${cl(values.phone)}</a></h2>
+        <h2><a class="website" href="${values.website_url}" target="_blank">${cl(values.website_url)}</a></h2>
+    </div>`).join('')
+  
+}
+
+main()
+})
+
